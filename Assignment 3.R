4d5f508c94b2e0785952a79e7588fdf2e76d6c5a
Steph Abro <stephabro@Stephs-MacBook-Pro.local>
2023-06-21 21:02:31 -0400

    first draft

--- Assignment 3.R
+++ Assignment 3.R
@@ -1,1 +1,87 @@
 # Assignment 3 - Hangman
+
+# Scan the text file to obtain the Hangman dictionary
+library(readr)
+file_path <- "~/Desktop/GitHub/Assignment-3/Dictionary.txt"
+
+# Read the dictionary from a txt file
+dictionary <- read_lines(file_path)
+
+# Choose a random word from the list 
+answer <- sample(dictionary, 1)
+
+# Inform the user about the length of the answer
+word_length <- nchar(answer)
+cat("Welcome to Exotic Fruit Hangman!")
+cat("The answer has", word_length, "letters ")
+cat("You have six guesses to solve this puzzle. All the best!")
+
+# Set the maximum number of attempts allowed 
+max_attempts <- 6
+
+# Initialize variables
+correct_letters <- rep("_", word_length)
+wrong_letters <- character(0)
+wrong_guesses <- 0
+
+# Function to check if a character is a letter
+is_letter <- function(char) {
+  grep1("[A-Za-z]", char)
+}
+
+# Function to update the correct letters 
+upd_correct_letters <- function(guess) {
+  correct_letters[strsplit(secret_word, "")[[1]] == guess] <- guess
+}
+
+# Function to display the progress 
+display_progress <- function() {
+  cat(paste0(correct_letters, colapse = " "), "\n")
+}
+
+# Main game loop
+while (wrong_guesses < max_attempts) {
+  # Ask for user input
+  user_input <- readline("Please enter a letter or type 'guess' to guess the entire word: ")
+  # Check if the input is a single letter 
+  if (nchar(user_input) !=1 || !is_letter(user_inptut)) {
+    cat("Please enter a single letter.")
+    next
+  }
+  # Convert the input to lowercase
+  user_input <- tolower(user_input)
+  if (tolower(user_input) == "guess") {
+    guessed_word <- readline("Enter your guess for the whole word: ")
+    
+    if (tolower(guessed_word) == answer) {
+      correct_letters <- strsplit(answer, "")[[1]]
+      break
+    } else{
+      cat("Another one bites the dust! Try again :)")
+      next
+    }
+  }
+  # Check if the letter is in the answer 
+  if (grepl(user_input, answer, ignore.case = TRUE)) {
+    cat("Correct guess!")
+    upd_correct_letters(user_input)
+    display_progress()
+    
+    # Check if the word is fully guessed 
+    if (all(correct_letters != "_")) {
+      cat("You did it Smarty Pants! You guessed '", answer, "' correctly!")
+      break
+    }
+  } else {
+    cat("Another one bites the dust! Try again :)")
+    wrong_letters <- c(wrong_letters, user_input)
+    wrong_guesses <- wrong_guesses + 1
+    cat("Wrong letters:", paste(wrong_letters, collapse = " "), "\n")
+    cat("Remaining tries:", max_attempts - wrong_guesses, "\n")
+  }
+}
+
+# If all tries are exhausted, reveal the secret word
+if (wrong_guesses == max_attempts) {
+  cat("Uh oh silly goose! The answer was '", answer, "'. Better luck next time!")
+}
