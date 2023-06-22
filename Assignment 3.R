688a7bc16037efac0fac4ca8857606cb97e195a2
Steph Abro <stephabro@Stephs-MacBook-Pro.local>
2023-06-22 16:12:49 -0400

    Third FINAL Draft

--- Assignment 3.R
+++ Assignment 3.R
@@ -4,38 +4,38 @@
 library(readr)
 file_path <- "~/Desktop/GitHub/Assignment-3/Dictionary.txt"
 
-# Read the dictionary from a txt file
+# Read the dictionary from the Dictionary.txt file
 dictionary <- read_lines(file_path)
 
-# Choose a random word from the list 
+# Choose a random word from the list in the txt file
 answer <- sample(dictionary, 1)
 
-# Inform the user about the length of the answer
+# Inform the user about the length of the answer and print the beginning messages which list the topic (Exotic Fruits), the word length of the answer (4 - 13 letters), and the # of attempts the user has (6)
 word_length <- nchar(answer)
 print("Welcome to Exotic Fruit Hangman!")
 print(paste("The answer has", word_length, "letters."))
 print("You have six attempts to solve this puzzle. All the best!")
 
-# Set the maximum number of attempts allowed 
+# Set the maximum number of attempts allowed; 6 attempts for each round
 max_attempts <- 6
 
-# Initialize variables
-correct_letters <- rep("_", word_length)
-wrong_letters <- character(0)
-wrong_guesses <- 0
+# Initialize the following variables
+correct_letters <- rep("_", word_length) # this line allows the user to visualize the word in this format: " _ _ _ _ "
+wrong_letters <- character(0) # initializes the wrong_letters variable as an empty character vector so that later the incorrectly guessed letters can be added to it using the c() function
+wrong_attempts <- 0 # initializes the wrong_attempts variable beginning at 0 so that later the incorrect number of attempts will be added using "wrong attempts + 1" with a limit at 6 (max amt of attempts)
 
 # Function to check if a character is a letter
-is_letter <- function(char) {
+is_letter <- function(char) { 
   grepl("[A-Za-z]", char)
 }
 
-# Function to display the progress
+# Function to display the progress - within the main game loop display_progress will print the current state of the game with filled-in letters and underscores to provide feedback to the user
 display_progress <- function() {
   print(paste(correct_letters, collapse = " "))
 }
 
 # Main game loop
-while (wrong_guesses < max_attempts) {
+while (wrong_attempts < max_attempts) {
   # Ask for user input
   user_input <- readline("Please enter a letter or type 'guess' to guess the entire word: ")
   # Convert the input to lowercase
@@ -51,7 +51,7 @@ while (wrong_guesses < max_attempts) {
       break
     } else {
       print("Wrong guess! Try again :)")
-      wrong_guesses <- wrong_guesses + 1  # Deduct an attempt for wrong word guess
+      wrong_attempts <- wrong_attempts + 1  # Deduct an attempt for wrong word guess
       print(paste("Remaining tries:", max_attempts - wrong_guesses))
       next
     }
@@ -81,13 +81,13 @@ while (wrong_guesses < max_attempts) {
   } else {
     print("Another one bites the dust! Try again :)")
     wrong_letters <- c(wrong_letters, user_input)
-    wrong_guesses <- wrong_guesses + 1
+    wrong_attempts <- wrong_attempts + 1
     print(paste("Wrong letters:", paste(wrong_letters, collapse = " ")))
-    print(paste("Remaining attempts:", max_attempts - wrong_guesses))
+    print(paste("Remaining attempts:", max_attempts - wrong_attempts))
   }
 }
 
 # If all tries are exhausted, reveal the secret word
-if (wrong_guesses == max_attempts) {
+if (wrong_attempts == max_attempts) {
   print(paste("Uh oh silly goose! The answer was '", answer, "'. Better luck next time!"))
 }
